db3936bf33492b1041f999b295f6725274cda82e
Anurag30112003 <anuragsharma011011@gmail.com>
2022-02-14 15:28:28 +0530

    coulmn adding done

--- management.py
+++ management.py
@@ -16,7 +16,7 @@ mycursor = mydb.cursor()
 # mycursor.execute("CREATE DATABASE IF NOT EXISTS hotel_management")
 # mycursor.execute("USE hotel_management")
 
-
+# table_name = None
 def create_table():
     table_name = input("Enter the table name: ")
     enter_columns = input("Enter the columns: ")
@@ -31,13 +31,21 @@ def add_columns(response, table_name):
     response = response.upper()
     if response == "Y":
         enter_columns = input("Enter the column name : ")
-        mycursor.execute(f"ALTER TABLE hotel_management.{table_name} ADD COLUMN {enter_columns} VARCHAR(255))")
+        mycursor.execute(f"ALTER TABLE hotel_management.{table_name} ADD COLUMN {enter_columns} VARCHAR(255)")
         print(f"Added {enter_columns} successfully")
+    elif response=="YES" and table_name == None :    
+        table_name = input("Enter the table name: ")
+        enter_columns = input("Enter the column name : ")
+        mycursor.execute(f"ALTER TABLE {table_name} ADD COLUMN {enter_columns} VARCHAR(255)")
+        print(f"Added {enter_columns} successfully")
+        start() 
     else:
         print("Thank you")
     
+
 def start():
     print("""
+    What do you want to do?
     1. Create Table
     2. Add Columns
     3. Exit
@@ -46,12 +54,15 @@ def start():
     if response == "1":
         create_table()
     elif response == "2":
-        add_columns()
-    else:
+        add_columns(table_name=None,response="YES")
+    elif response == "3":
         print("Thank you")
+    else:
+        print("Invalid Input !!")
+        start()   
 start()
         
-    
+
     
 
 
