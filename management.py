88945e79d0f170769234870268907d7b4283a2e8
Anurag30112003 <anuragsharma011011@gmail.com>
2022-02-14 15:31:22 +0530

    improve

--- management.py
+++ management.py
@@ -31,16 +31,20 @@ def add_columns(response, table_name):
     response = response.upper()
     if response == "Y":
         enter_columns = input("Enter the column name : ")
-        mycursor.execute(f"ALTER TABLE hotel_management.{table_name} ADD COLUMN {enter_columns} VARCHAR(255)")
+        mycursor.execute(f"ALTER TABLE {table_name} ADD COLUMN {enter_columns} VARCHAR(255)")
         print(f"Added {enter_columns} successfully")
+        start()
     elif response=="YES" and table_name == None :    
         table_name = input("Enter the table name: ")
         enter_columns = input("Enter the column name : ")
         mycursor.execute(f"ALTER TABLE {table_name} ADD COLUMN {enter_columns} VARCHAR(255)")
         print(f"Added {enter_columns} successfully")
         start() 
-    else:
+    elif response == "N":
         print("Thank you")
+    else:
+        print("Invalid Input !!")
+        add_columns(response, table_name)
     
 
 def start():
