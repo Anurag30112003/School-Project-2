82883cf07eb3dba1dcc3fcbdf0b719d35b40bd63
Anurag30112003 <anuragsharma011011@gmail.com>
2022-02-14 19:04:28 +0530

    added

--- code/start.py
+++ code/start.py
@@ -1,25 +1,51 @@
 from table_create import create_table
 from columns_add import add_columns
+from dotenv import load_dotenv
+import os
+load_dotenv()
+adminpass = os.getenv("ADMIN_PASS")
 leave = False
 # Hotel Manangement system in python with mysql database.
 def start(leave):
     while leave == False:
+        
         print("""
         What do you want to do?
-        1. Create Table
-        2. Add Columns
+        1. Admin Section
+        2. Add New room
         3. Exit
         """)
         response = input("Enter your choice : ")
         if response == "1":
-            create_table()
+            p = input("Enter your password : ")
+            if p == adminpass:
+                print("Welcome to Admin Section")
+                adminleave = False
+                while adminleave == False:
+                    print("""
+                    What do you want to do?
+                    1. Create Table
+                    2. Add Columns
+                    3. Exit
+                    """)
+                    response = input("Enter your choice : ")
+                    if response == "1":
+                        create_table()
+                    elif response == "2":
+                        add_columns(table_name=None,response="YES")
+                    elif response == "3":
+                        print("Thank you !")
+                        adminleave = True
+                    else:
+                        print("Invalid Input !!")
+                        start(leave=False)   
         elif response == "2":
-            add_columns(table_name=None,response="YES")
+            print("hello room")
+            pass
         elif response == "3":
-            print("Thank you !")
             leave = True
         else:
             print("Invalid Input !!")
-            start(leave=False)   
+            start(leave=False)
 start(leave)
         
