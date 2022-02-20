8f4cacf0fbbd3f8985ea355b95398459b4ff953f
Anurag30112003 <anuragsharma011011@gmail.com>
2022-02-21 01:37:04 +0530

    Table show

---
+++ code/databaseshow.py
@@ -0,0 +1,20 @@
+from config import mydb , mycursor
+
+def showdatabases():
+    response = input("Enter the table from these (1. Rooms\ 2. Customers) - ")
+    if response == '1':
+        mycursor.execute('SELECT * FROM rooms')
+        b = mycursor.fetchall()
+        for i in b:
+            print("\n",i)
+            print("\n")
+            print("============================================================================")
+    elif response == '2':
+        mycursor.execute('SELECT * FROM customers')
+        b = mycursor.fetchall()
+        for i in b:
+            print("\n",i)
+            print("\n")
+            print("============================================================================")
+
+
