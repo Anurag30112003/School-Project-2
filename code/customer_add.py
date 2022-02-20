12846bd14ae74e8864a32f666c4507c2f122a96e
Anurag30112003 <anuragsharma011011@gmail.com>
2022-02-21 02:03:04 +0530

    improve

--- code/customer_add.py
+++ code/customer_add.py
@@ -3,8 +3,8 @@ from config import mydb , mycursor
 def add_customer():
     name = input("Enter name: ")
     phone_number = input("Enter phone: ")
+    email = input("Enter Email: ")
     address = input("Enter address: ")
-    email = input("Enter email: ")
     password = input("Make password: ")
     sql = "INSERT INTO customers (name, phone_number, address, email,password) VALUES (%s, %s, %s , %s,%s)" 
     val = (name, phone_number, address, email,password)
