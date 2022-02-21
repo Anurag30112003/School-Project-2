d3c61976e162ef6e12b88ac19ae12a8cc9181aed
Anurag30112003 <anuragsharma011011@gmail.com>
2022-02-22 02:02:38 +0530

    checks

--- code/customer_add.py
+++ code/customer_add.py
@@ -1,13 +1,23 @@
 from config import mydb , mycursor
 
 def add_customer():
-    name = input("Enter name: ")
-    phone_number = input("Enter phone: ")
-    email = input("Enter Email: ")
-    address = input("Enter address: ")
-    password = input("Make password: ")
-    sql = "INSERT INTO customers (name, phone_number, address, email,password) VALUES (%s, %s, %s , %s,%s)" 
-    val = (name, phone_number, address, email,password)
-    mycursor.execute(sql, val)
-    mydb.commit()
-    print(f"Customer {name} added successfully")
+        name = input("Enter name: ")
+        while True:
+            phone_number = input("Enter phone: ")
+            if len(phone_number) == 10:
+                break
+            else:
+                print("Please enter a valid phone number")       
+        while True:
+            email = input("Enter Email: ")
+            if '@' in email:
+                address = input("Enter address: ")
+                password = input("Make password: ")
+                sql = "INSERT INTO customers (name, phone_number, address, email,password) VALUES (%s, %s, %s , %s,%s)" 
+                val = (name, phone_number, address, email,password)
+                mycursor.execute(sql, val)
+                mydb.commit()
+                print(f"Customer {name} added successfully")
+                break
+            else:
+                print("Invalid email")
--- code/customer_delete.py
+++ code/customer_delete.py
@@ -1,9 +1,15 @@
 from config import mydb, mycursor
 def delete_customer():
     username = input("Enter name: ")
-    sql = "DELETE FROM customers WHERE name = %s"
-    val = (username,)
-    mycursor.execute(sql,val)
-    mydb.commit()
-    print(mycursor.rowcount, "record(s) deleted")
+    mycursor.execute("SELECT name FROM customers WHERE name = %s", (username,))
+    customer_name_check = mycursor.fetchone()
+    customer_name_check = str(customer_name_check).replace("(", "").replace(")", "").replace(",", "").replace("'", "")
+    if customer_name_check == 'None':
+        print("Customer does not exist")
+    else:
+        sql = "DELETE FROM customers WHERE name = %s"
+        val = (username,)
+        mycursor.execute(sql,val)
+        mydb.commit()
+        print(mycursor.rowcount, "record(s) deleted")
 
